d4b8c6eba56ca721b591a67ff6f7fc5d987323b0
Matias Tite <matiastite1@gmail.com>
2023-06-17 17:51:26 -0500

    With this push ill add some changes on the views, directories, and i added some external libraries like dataTable

--- SalesPoint/SalesPoint/core/erp/urls.py
+++ SalesPoint/SalesPoint/core/erp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from SalesPoint.core.erp.views.category.views import category_list
+from SalesPoint.core.erp.views.category.views import *
 
 urlpatterns = [
-    path('categoria/listado', category_list, name='category_list'),
+    path('categoria/listado', CategoryListView.as_view(), name='category_list'),
 ]
--- SalesPoint/SalesPoint/core/erp/views/category/views.py
+++ SalesPoint/SalesPoint/core/erp/views/category/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from SalesPoint.core.erp.models import Category
+from django.views.generic import ListView
 
 def category_list(request):
     data = {
         'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+    

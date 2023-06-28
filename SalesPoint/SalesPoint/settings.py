2732e21fe952620ac8fb9214fdfe4f4a226a5157
Matias Tite <matiastite1@gmail.com>
2023-06-28 02:43:19 -0500

    Commit with the homepage app

--- SalesPoint/SalesPoint/settings.py
+++ SalesPoint/SalesPoint/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "widget_tweaks",
     "SalesPoint.core.erp",
+    "SalesPoint.core.homepage",
 ]
 
 MIDDLEWARE = [
--- SalesPoint/SalesPoint/urls.py
+++ SalesPoint/SalesPoint/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from SalesPoint.core.homepage.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("erp/", include("SalesPoint.core.erp.urls")),
+    path("", IndexView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
